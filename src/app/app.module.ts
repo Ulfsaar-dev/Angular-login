72029dcda666fd66e755acdc3662a3626fb25ca4
Ulfsaar-dev <wangtoby1125@outlook.com>
2020-01-28 23:01:33 +0800

    LoginForm created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { DemoMaterialModule } from './material.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
